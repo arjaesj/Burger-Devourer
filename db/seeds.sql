d6b15393033dfdb75a3417c2df633725c908f273
Rommel Jay San Jose <arjae.sj@gmail.com>
2020-08-04 23:32:19 +0800

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('BLT');
+INSERT INTO burgers (burger_name) VALUES ('Baa Baa Burger');
+INSERT INTO burgers (burger_name) VALUES ('Wagyu Wunder');
+INSERT INTO burgers (burger_name) VALUES ('Sir Truffle');
+INSERT INTO burgers (burger_name) VALUES ('BBQ Brisket');
