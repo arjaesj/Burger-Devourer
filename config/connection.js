8378292bcfe050e845bc0e489a6775312e1abfa0
Rommel Jay San Jose <arjae.sj@gmail.com>
2020-08-08 22:18:29 +0800

    added connection credetils for Heroku publishing

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 var mysql = require("mysql");
 require("dotenv").config();
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: process.env.PASSWORD,
-    database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWS_URL) {
+    connection = mysql.createConnection(process.env.JAWS_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: process.env.PASSWORD,
+        database: "burgers_db"
+    });
+};
 
 
 connection.connect(function(error) {
