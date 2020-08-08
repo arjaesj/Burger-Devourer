12043c25785c50361c7caa98c5e6dbf3037d7af7
Rommel Jay San Jose <arjae.sj@gmail.com>
2020-08-08 22:59:25 +0800

    updated connection credentials

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 
 var connection;
 
-if (process.env.JAWS_URL) {
-    connection = mysql.createConnection(process.env.JAWS_URL);
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
         host: "localhost",
